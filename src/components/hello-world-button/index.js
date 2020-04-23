064b2ae59effc9cfdc9b6cceeb250eb55eb9ff67
Wilson Neto <wilsonpsn@hotmail.com>
2020-04-23 14:15:27 -0300

    Extract CSS

--- src/components/hello-world-button/index.js
+++ src/components/hello-world-button/index.js
@@ -1,7 +1,7 @@
 import './hello-world-button.scss';
 
 class HelloWorldButton {
-  buttonCssClass = 'hello-world-button'
+  buttonCssClass = 'hello-world-button';
   render() {
     const button = document.createElement('button');
     button.innerHTML = 'Hello World';
@@ -11,7 +11,7 @@ class HelloWorldButton {
       p.classList.add('hello-world-text');
       body.appendChild(p);
     };
-    
+
     const body = document.querySelector('body');
     button.classList.add(this.buttonCssClass);
     body.appendChild(button);
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import HelloWorldButton from './components/hello-world-button';
+import Heading from './components/heading';
 import addImage from './add-image.js';
+
+const heading = new Heading();
+heading.render();
 
 const helloWorldButton = new HelloWorldButton();
 helloWorldButton.render();
