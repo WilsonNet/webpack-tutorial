550c79d4b901b5dbcabee198c003af698f10cbf1
Wilson Neto <wilsonpsn@hotmail.com>
2020-04-23 15:14:38 -0300

    Prod and dev scripts created

--- src/index.js
+++ src/index.js
@@ -7,3 +7,10 @@ heading.render();
 
 const helloWorldButton = new HelloWorldButton();
 helloWorldButton.render();
+
+if (process.env.NODE_ENV === 'production') {
+  console.log('prod');
+} else if (process.env.NODE_ENV === 'development') {
+  console.log('dev');
+}
+
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,17 +1,16 @@
 const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle.[contenthash].js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, './dist'),
     publicPath: '',
   },
-  mode: 'none',
+  mode: 'development',
   module: {
     rules: [
       {
@@ -20,11 +19,11 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
         test: /\.js$/,
@@ -44,10 +43,6 @@ module.exports = {
     ],
   },
   plugins: [
-    new TerserPlugin(),
-    new MiniCssExtractPlugin({
-      filename: 'styles.[contenthash].css',
-    }),
     new CleanWebpackPlugin({
       cleanOnceBeforeBuildPatterns: [
         '**/*',
